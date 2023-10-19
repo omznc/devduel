110abce82cd1377b8efcbc5695677141d4369b49
omznc <contact@omarzunic.com>
2023-10-19 23:14:27 +0200

    fix: profile set username

--- src/app/u/[username]/layout.tsx
+++ src/app/u/[username]/layout.tsx
@@ -1,6 +1,7 @@
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@app/api/auth/[...nextauth]/route.ts';
 import { ReactNode } from 'react';
+import { redirect } from 'next/navigation';
 
 // @ts-ignore
 export default async function Layout({ me, other, params }: {
@@ -12,7 +13,7 @@ export default async function Layout({ me, other, params }: {
 }) {
 	// @ts-ignore
 	const session = await getServerSession(authOptions);
-	if (session && session?.user?.username && ["me", params.username].includes(session?.user?.username)) {
+	if (session && (!session?.user?.username || ["me", params.username].includes(session?.user?.username))) {
 		return me;
 	}
 	return other
