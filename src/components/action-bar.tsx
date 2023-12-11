0b92ec4a4530e57c8c774f0e3e8b31e17b37a554
omznc <contact@omarzunic.com>
2023-12-11 22:28:44 +0100

    added className toa ctionbar

--- src/components/action-bar.tsx
+++ src/components/action-bar.tsx
@@ -1,8 +1,11 @@
 import { ReactNode } from "react";
+import { cn } from '@lib/utils.ts';
 
-export default function ActionBar({ children }: { children: ReactNode }) {
+export default function ActionBar({ children, className }: { children: ReactNode, className?: string }) {
 	return (
-		<div className="flex fly-in-from-top w-full max-w-4xl flex-row flex-wrap items-center justify-center gap-4">
+		<div className={
+			cn("flex fly-in-from-top w-full max-w-4xl flex-row flex-wrap items-center justify-center gap-4", className)
+		}>
 			{children}
 		</div>
 	);
